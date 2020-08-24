ff4bca2fc4cdcf4e8e6c507a56b0f579e6a74e4c
Andrii Matviienko <64703622+andymtv@users.noreply.github.com>
2020-08-24 18:39:52 +0200

    Minor changes
    Popup banner was added. Subscription form already work.

--- js/main.js
+++ js/main.js
@@ -28,5 +28,38 @@ window.addEventListener('DOMContentLoaded', function() {
                     $item.css({"list-style-type" : "none", "display" : "flex"})
             }
         })
+    })   
+
+    $('.btn-subscribe-disc').click((e) => {
+        e.preventDefault();
+        sbscrInput = $('.input-disc').val();
+        let at = sbscrInput.indexOf('@');
+        let dot = sbscrInput.indexOf('.');
+        let lastChar = sbscrInput.charAt(sbscrInput.length - 1);
+        let lastCharCode = lastChar.charCodeAt(0);
+        let $msg = $('.msg-text');
+        if(sbscrInput == null || sbscrInput == "" || sbscrInput == undefined){
+            $msg.text('This field cannot be empty! Please type your email!');
+            $msg.show(0);
+        } else if(at < 1) {
+            $msg.text("Email must contain '@'.");
+            $msg.show(0);
+        } else if(dot < 1) {
+            $msg.text("Email must contain '.'.");
+            $msg.show(0);
+        }else if((lastCharCode >= 97 && lastCharCode <= 122) || (lastCharCode >= 65 && lastCharCode <= 90)) {
+            $('.input-disc').val(' ');
+            $('.modal').fadeToggle(300);
+            $('.modal').addClass('active');
+        } else{ 
+            $msg.text("Email incorect. Please, type email in format: qwer12@qwe.com");
+            $msg.show(0);
+
+        }
     })
 });
+
+$('.modal-submit').click(() => {
+    $('.modal').removeClass('active');
+    $('.modal').fadeToggle(300);
+})
